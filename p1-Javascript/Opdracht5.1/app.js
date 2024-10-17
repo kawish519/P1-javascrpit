b442922c34ba5e15c2d63ae9a9c6529641b4998c
kawish519 <9025234@student.zadkine.nl>
2024-10-17 12:47:58 +0200

    Add files via upload

---
+++ p1-Javascript/Opdracht5.1/app.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    var HeadingtoAdjust = document.getElementById("TestHeader2");
+    HeadingtoAdjust.style.color = "Blue";
+    document.getElementById("TestHeader1").innerHTML = "New tekst!";
+}
